144bdd53021e497aaccf880fba0a46eddb347758
mrc <marcoambrosini@pm.me>
2019-05-08 11:05:45 +0200

    vuex state and getters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <p>{{ getEventByID(2) }}</p>
+
+    <ul>
+      <li v-for="(category, index) in categories" :key="`${category}-${index}`">
+        {{ category }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    getEvent() {
+      return this.$store.getters.getEventByID
+    },
+    ...mapGetters(['getEventByID']),
+    ...mapState(['user', 'categories'])
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
